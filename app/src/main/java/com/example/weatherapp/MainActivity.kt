57a5a980fa417fc85e0ea744f4cdd9f4f4bf93f9
AdamBoba <adamekboba@gmail.com>
2024-04-08 22:35:41 +0200

    dodanie aplikacji

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    private val apiKey = "ec863e8d82abb802938a191603df2ae4"
+    private val weatherBaseUrl = "https://api.openweathermap.org/data/2.5/weather?"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+            val city = editText.text.toString()
+            if (city.isNotEmpty()) {
+                getWeatherByCity(city, textView)
+            }
+        }
+    }
+
+    private fun getWeatherByCity(city: String, textView: TextView) {
+        val weatherUrl = "$weatherBaseUrl&q=$city&appid=$apiKey&units=metric"
+
+        val request = Request.Builder().url(weatherUrl).build()
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                runOnUiThread {
+                    textView.text = "Błąd podczas pobierania danych pogodowych"
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                val json = JSONObject(body)
+                if (json.has("main") && json.has("weather")) {
+                    val main = json.getJSONObject("main")
+                    val temperature = main.getDouble("temp")
+                    val weatherArray = json.getJSONArray("weather")
+                    val weatherObject = weatherArray.getJSONObject(0)
+                    val description = weatherObject.getString("description")
+
+                    val result = "Temperatura w $city: $temperature°C\nOpis: $description"
+
+                    runOnUiThread {
+                        textView.text = result
+                    }
+                } else {
+                    runOnUiThread {
+                        textView.text = "Nie udało się znaleźć danych dla podanego miasta"
+                    }
+                }
+            }
+        })
+    }
+}
